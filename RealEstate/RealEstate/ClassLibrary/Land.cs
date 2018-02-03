1571880ccd81450317a13eb308547ccb018f6720
Seth Cleaver <sethcleaver15@gmail.com>
2018-02-02 18:38:32 -0700

    2/5/18

---
+++ RealEstate/RealEstate/ClassLibrary/Land.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+   public class Land
+    {
+        //Properties go here
+        private ushort _PropertySize;
+        public ushort Propertysize { get; set; }
+
+        private bool _WaterRights;
+        public bool WaterRights { get; set; }
+
+        private bool _FarmLand;
+        public bool FarmLand { get; set; }
+
+        private bool _buildingLot;
+        public bool Buildinglot { get; set; }
+
+
+        //Methods Go Here
+
+    }
+}
